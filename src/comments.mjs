9f971539027619aecc91c0640cb908c7c2377633
Sam Stenvall <sam.stenvall@nitor.com>
2024-08-30 14:11:25 +0300

    Fix typo

--- src/comments.mjs
+++ src/comments.mjs
@@ -2,7 +2,7 @@ const Comments = [
     'Markkina on puhunut!',
     'Mistä näitä senttejä oikein tulee?',
     'Taivas varjele, mitä sieltä tulee? Sieltä tulee hi-nnat!',
-    'Se nyt vaan on tyhmää maksaa liikaa!F'
+    'Se nyt vaan on tyhmää maksaa liikaa!'
 ]
 
 export const getRandomComment = () => {
