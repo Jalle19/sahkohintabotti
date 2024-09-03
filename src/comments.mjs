4571084dd2e6964c3cf9fc8f48edf49c4f9a44f8
Sam Stenvall <sam.stenvall@nitor.com>
2024-09-03 12:51:40 +0300

    New comment

--- src/comments.mjs
+++ src/comments.mjs
@@ -2,7 +2,8 @@ const Comments = [
     'Markkina on puhunut!',
     'Mistä näitä senttejä oikein tulee?',
     'Taivas varjele, mitä sieltä tulee? Sieltä tulee hi-nnat!',
-    'Se nyt vaan on tyhmää maksaa liikaa!'
+    'Se nyt vaan on tyhmää maksaa liikaa!',
+    'Have you heard about the miracle of pörssisähkö prices?',
 ]
 
 export const getRandomComment = () => {
