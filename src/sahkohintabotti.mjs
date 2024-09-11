64a69b9f56e501a7a0a279efd00c88f353592014
Sam Stenvall <sam.stenvall@nitor.com>
2024-09-11 14:15:01 +0300

    Fall back to Yle image if we fail to get the correct page

--- src/sahkohintabotti.mjs
+++ src/sahkohintabotti.mjs
@@ -3,6 +3,8 @@ import { execFileSync } from 'node:child_process'
 import yargs from 'yargs'
 import { WebClient } from '@slack/web-api'
 import {getRandomComment} from './comments.mjs'
+import * as fs from 'node:fs'
+import { Readable, finished } from 'stream'
 
 const argv = yargs(process.argv.slice(2))
   .usage('node $0 [options]')
@@ -30,6 +32,7 @@ const argv = yargs(process.argv.slice(2))
   console.log(`Downloading ${ttiUrl}`)
   const response = await fetch(ttiUrl)
   let body = await response.text()
+  const originalBody = body
 
   // Replace some strings
   body = body.replace('YLE TEKSTI-TV', 'NITOR SLACK')
@@ -45,6 +48,19 @@ const argv = yargs(process.argv.slice(2))
   execFileSync('/usr/local/bin/TTI2IMG', ['-i', ttiPath, '-o', argv.outputPath])
   const pngFilePath = `${argv.outputPath}/${argv.ttiPage}-0.png`
   console.log(`Output file is ${pngFilePath}`)
+
+  // Fall back to Yle image if parsing failed
+  if (!originalBody.includes("Nord Pool")) {
+    const appId = process.env.YLE_APP_ID
+    const appKey = process.env.YLE_APP_KEY
+
+    const yleResponse = await fetch(`https://external.api.yle.fi/v1/teletext/images/189/1.png?app_id=${appId}&app_key=${appKey}`)
+    const readStream = Readable.fromWeb(yleResponse.body)
+    const stream = fs.createWriteStream(pngFilePath)
+
+    await readStream.pipe(stream)
+    console.log(`Fell back to YLE image`)
+  }
 
   // Post to Slack
   const channelId = process.env.CHANNEL_ID
