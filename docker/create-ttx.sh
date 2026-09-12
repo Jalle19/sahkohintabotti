8c5c37f001707d44410adc926f926cd0de9685c8
Sam Stenvall <sam.stenvall@nitor.com>
2026-09-12 13:37:14 +0300

    Add script for converting MPEG-TS to TTX files

---
+++ docker/create-ttx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+rm -rf "$TTX_OUTPUT_DIRECTORY"
+mkdir -p "$TTX_OUTPUT_DIRECTORY"
+
+/usr/local/bin/TtxFromTS \
+  -i "$MPEGTS_OUTPUT_PATH" \
+  --pid "$TELETEXT_PID" \
+  --no-config \
+  -o "$TTX_OUTPUT_DIRECTORY"
+
+NUM_TTX_FILES=$(ls -1q "$TTX_OUTPUT_DIRECTORY" | wc -l)
+
+test $NUM_TTX_FILES -gt 0
