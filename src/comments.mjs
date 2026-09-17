964f223aa95a22be7b3eb5ffd11ab098234e99c4
Sam Stenvall <sam.stenvall@nitor.com>
2026-09-17 10:14:21 +0300

    Refresh slogans

--- src/comments.mjs
+++ src/comments.mjs
@@ -6,7 +6,8 @@ const Comments = [
     'Pörssisähkö - todennäköisesti aina halvempi',
     'Have you heard about the miracle of ~Elisa Saunalahti~ pörssisähkö prices?',
     'Live by the pörssi, die by the pörssi',
-    'Alussa oli vain suo, kuokka – ja sähköpörssi',
+    "Pörssisähkö - because you're worth it",
+    "_Choose the pörssi and you will join me_\n_Choose the kiinteä, and you join your mother in death_\n_You don't understand my words, but you must choose_\n_So, come boy, choose life or death_"
 ]
 
 export const getRandomComment = () => {
